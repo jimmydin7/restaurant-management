320cae156551939403d4ecdae107e5194884d588
jimmydin7 <turnspeaker@gmail.com>
2025-10-31 20:05:13 +0200

    app run function and options

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,4 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-app.run(debug=True)
+
+
+def app_run(alt_port, dev_mode=False):
+    if dev_mode:
+        app.debug = True
+    port = int(os.getenv("PORT", alt_port))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == '__main__':
+    app_run(5000, dev_mode=True)
